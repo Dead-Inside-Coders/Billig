4eb7274742a5bff2a6e54b6c95b040cca4740e42
Roma <roma.ivanov1999@mail.ru>
2021-01-24 02:23:53 +0300

    add date

--- Integral/Integral/Integral.cpp
+++ Integral/Integral/Integral.cpp
@@ -9,38 +9,40 @@
 
 inline double F(double x)
 {
-	return pow(x, 2) * sin(x); //cos(sin(x * x) - x * x * x + x * x * x * x / 47 - 54 * cos(pow(x, -1.5)));
+	return cos(sin(x * x) - x * x * x + x * x * x * x / 47 - 54 * cos(pow(x, -1.5))); //pow(x,2)*sin(x)
 }
 
-int main()
-{
+long getIntegralResult(const double begin, const double end, const long steps) {
+
 	auto start_time = std::chrono::high_resolution_clock::now();
 
-	const double begin = 1;// 1; //граници интегрирования 
-	const double end = 10;//200; //граници интегрирования 
-	const long int values_count = 1e7; //шаг 
+	//const double begin = 1;// 1; //граници интегрирования 
+	//const double end = 10;//200; //граници интегрирования 
+	//const long values_count = 1e7; //шаг 
 
-	double* web = new double[values_count]; 
+	double* web = new double[steps];
 	web[0] = begin;
+
+	//omp_set_num_threads(30); // число потоков
 
 #pragma omp parallel
 	{
 #pragma omp for
-		for (int i = 1; i < values_count - 1; ++i) {
-			web[i] = begin + ((end - begin) / (values_count - 1)) * i;
+		for (int i = 1; i < steps - 1; ++i) {
+			web[i] = begin + ((end - begin) / (steps - 1)) * i;
 		}
 	}
-	web[values_count - 1] = end;
+	web[steps - 1] = end;
 	double res = 0;
 
 
-	//omp_set_num_threads(5); // число потоков 
+	 
 #pragma omp parallel
 	{
 		double a, b; //граници интегрирования 
 		double prores = 0; //результат
 #pragma omp for
-		for (int i = 0; i < (values_count - 1); ++i)
+		for (int i = 0; i < (steps - 1); ++i)
 		{
 			a = web[i];
 			b = web[i + 1];
@@ -55,8 +57,25 @@ int main()
 	auto stop_time = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop_time - start_time);
 
-	printf("Integral = %f\n", res);
-	printf("Time spent: %d", duration.count());
+	//printf("Integral = %f\n", res);
+	//printf("Time spent: %d\n", duration.count());
+
+	return duration.count();
+}
+
+int main()
+{
+	
+	for (int i = 4; i < 9; i++)
+	{
+		long results = 0;
+		for (int j = 0; j < 3; j++)
+		{
+			results += getIntegralResult(1, 10, pow(10,i));
+		}
+		printf("Avg time %d for epsilon 1e%d\n", results/2 ,i);
+	}
+	
 
 }
 
